593dbde36a4344ba62992fe023922e7f8535c957
Critiek <jumbra99@gmail.com>
2023-12-09 14:27:19 +1030

    neotree bindings l and h to open and close files/dirs

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  opts = {
+    window = {
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node",
+      },
+    },
+  },
+}
