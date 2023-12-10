0e6ca93231cd9451e14202ddbfd6d4a2830d9309
Critiek <jumbra99@gmail.com>
2023-12-10 18:44:09 +1030

    re-enabled neotree because it doesn't reeally matter and it juist works

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,6 +1,8 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
+  { "rose-pine/neovim", name = "rose-pine" },
+
   {
     "LazyVim/LazyVim",
     opts = {
