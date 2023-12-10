5e3d4f3a0143590c85ff5e4c851f567d2fde9dcf
Critiek <jumbra99@gmail.com>
2023-12-10 16:59:47 +1030

    disabled neo-tree because it is slow af

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,3 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  cmd = "Neotree",
-  opts = {
-    window = {
-      mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-      },
-    },
-  },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
