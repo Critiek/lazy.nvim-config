d99d1097e75e16130aa84d5163d1c89fed1580c0
Critiek <jumbra99@gmail.com>
2024-02-19 21:16:41 +1030

    colourscheme stuff

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,5 +1,5 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 
   { "rose-pine/neovim", name = "rose-pine" },
 
