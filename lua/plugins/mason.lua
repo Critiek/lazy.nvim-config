35220a086bf2023b509fd214bbd12c7f032dacdd
Critiek <jumbra99@gmail.com>
2024-03-05 22:13:05 +1030

    automatic treesitter and mason package installation

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "bash-language-server",
+        "css-lsp",
+        "html-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "marksman",
+        "powershell-editor-services",
+        "pyright",
+        "rust-analyzer",
+        "typescript-language-server",
+        "yaml-language-server",
+      },
+    },
+  },
+}
