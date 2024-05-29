553d45a7626ded5f5c113b2e34f7579455640863
Critiek <jamesvsomeren@gmail.com>
2024-05-29 22:19:15 +0930

    tab settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,8 @@ vim.opt.termguicolors = true
 vim.cmd.set("relativenumber")
 
 vim.opt.scrolloff = 12
+
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
