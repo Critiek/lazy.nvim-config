8834ead90b92539c01b3a09c5ddea7ed4295c31f
Critiek <jamesvsomeren@gmail.com>
2024-06-01 18:26:36 +0930

    rainbow delims not in html because brokey

--- lua/plugins/rainbow_delimiters.lua
+++ lua/plugins/rainbow_delimiters.lua
@@ -1,5 +1,8 @@
 return {
-  {
     "hiphish/rainbow-delimiters.nvim",
-  },
+    init = function()
+        require("rainbow-delimiters.setup").setup({
+            blacklist = { "html" },
+        })
+    end,
 }
