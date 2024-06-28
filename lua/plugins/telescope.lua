a96cc43ddfd0a1cdb54053a4c8beda87fdb3a0a8
Critiek <jamesvsomeren@gmail.com>
2024-06-29 03:08:51 +0930

    fixed telescope bull

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,5 +3,8 @@ return {
     keys = {
         -- disable the keymap to grep files
         { "<leader>/", false },
+        { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+        { "<leader>fF", false },
     },
 }
