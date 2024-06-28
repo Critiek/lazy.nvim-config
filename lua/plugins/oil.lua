6f1d10713fbb028a58a669ecc8e39be2849917df
Critiek <jamesvsomeren@gmail.com>
2024-06-28 19:42:57 +0930

    changed oil keybindings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,14 +3,14 @@ return {
     opts = {
         keymaps = {
             ["g?"] = "actions.show_help",
-            ["<S-l>"] = "actions.select",
+            ["l"] = "actions.select",
             ["<C-s>"] = "actions.select_vsplit",
             ["<C-h>"] = "actions.select_split",
             ["<C-t>"] = "actions.select_tab",
             ["<C-p>"] = "actions.preview",
             ["<C-c>"] = "actions.close",
             ["<C-l>"] = "actions.refresh",
-            ["<S-h>"] = "actions.parent",
+            ["h"] = "actions.parent",
             ["_"] = "actions.open_cwd",
             ["`"] = "actions.cd",
             ["~"] = "actions.tcd",
