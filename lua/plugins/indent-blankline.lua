a58116fd5aa0492abc215291a8d6e5e7d1337351
Critiek <jamesvsomeren@gmail.com>
2024-07-13 21:57:44 +0930

    indent change

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,32 +1,54 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    opts = function(_, opts)
-        -- Other blankline configuration here
-        opts = {
-            indent = {
-                char = "│",
-                tab_char = "│",
+    opts = {
+        indent = {
+            char = ">",
+            tab_char = ">",
+        },
+        scope = { show_start = false, show_end = false },
+        exclude = {
+            filetypes = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
             },
-            scope = { show_start = false, show_end = false },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-            },
-        }
-        return require("indent-rainbowline").make_opts(opts)
-    end,
-    dependencies = {
-        "TheGLander/indent-rainbowline.nvim",
+        },
     },
+    -- opts = function(_, opts)
+    --     -- Other blankline configuration here
+    --     opts = {
+    --         indent = {
+    --             char = "│",
+    --             tab_char = "│",
+    --         },
+    --         scope = { show_start = false, show_end = false },
+    --         exclude = {
+    --             filetypes = {
+    --                 "help",
+    --                 "alpha",
+    --                 "dashboard",
+    --                 "neo-tree",
+    --                 "Trouble",
+    --                 "trouble",
+    --                 "lazy",
+    --                 "mason",
+    --                 "notify",
+    --                 "toggleterm",
+    --                 "lazyterm",
+    --             },
+    --         },
+    --     }
+    --     return require("indent-rainbowline").make_opts(opts)
+    -- end,
+    -- dependencies = {
+    --     "TheGLander/indent-rainbowline.nvim",
+    -- },
 }
