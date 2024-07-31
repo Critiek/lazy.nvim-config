90dad9002d71e4fbd3473c9f24a796efc3602699
Critiek <jamesvsomeren@gmail.com>
2024-08-01 01:13:33 +0930

    markdown preview works but requires removal and re-installation to update

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,13 +1,11 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    -- build = function()
-    --     vim.fn["mkdp#util#install"]()
-    -- end,
     build = "cd app && npm install",
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     keys = {
         {
             "<leader>cp",
