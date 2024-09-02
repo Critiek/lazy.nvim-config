c6c2ddedca89ce15089ee3311a050fbb502b6c56
Critiek <jamesvsomeren@gmail.com>
2024-09-02 21:38:58 +0930

    removed markdown because it was goofy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.toml" },
         { import = "lazyvim.plugins.extras.lang.json" },
-        { import = "lazyvim.plugins.extras.lang.markdown" },
+        -- { import = "lazyvim.plugins.extras.lang.markdown" },
         -- import/override with your plugins
         { import = "plugins" },
     },
