678b643f5c4f840e0be515be662d2d56bcbd90ba
Critiek <jamesvsomeren@gmail.com>
2024-09-04 00:28:13 +0930

    noice config and lazy-lock no longer in gitignore for version control between machines

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,3 +1,18 @@
 return {
-    { "folke/noice.nvim", enabled = false },
+    "folke/noice.nvim",
+    enabled = true,
+    opts = {
+        views = {
+            cmdline_popup = {
+                border = {
+                    style = "none",
+                    padding = { 1, 2 },
+                },
+                filter_options = {},
+                win_options = {
+                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                },
+            },
+        },
+    },
 }
