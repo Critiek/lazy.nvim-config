150bf6c8960d9d05985556fc984674f3d6519514
Critiek <jamesvsomeren@gmail.com>
2024-09-04 01:06:53 +0930

    ctrl+shift+v pasting keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,5 +8,8 @@ vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
 vim.keymap.set({ "n", "v" }, "x", '"_x')
 vim.keymap.set({ "n", "v" }, "X", '"_x')
 
+vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<C-S-v>", function()
+    vim.api.nvim_paste(vim.fn.getreg("+"), false, -1)
+end, { noremap = true, silent = true })
 -- vim.keymap.set({ "n", "v" }, "<leader>e", "<cmd>CHADopen<cr>")
 -- vim.keymap.set({ "n", "v" }, "<leader>e", "<cmd>Oil<cr>")
