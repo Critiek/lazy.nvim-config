d8e0b9d6f266d28af36f3f0763cafef3aa7cd71b
Critiek <jamesvsomeren@gmail.com>
2024-09-18 00:13:15 +0930

    rust analyzer instead

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.yaml" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.markdown" },
-        { import = "lazyvim.plugins.extras.lang.rust" },
+        -- { import = "lazyvim.plugins.extras.lang.rust" },
         -- import/override with your plugins
         { import = "plugins" },
     },
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,10 @@
+return {
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            rust_analyzer = {},
+            cssls = {},
+            html = {},
+        },
+    },
+}
