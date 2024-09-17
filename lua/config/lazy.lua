aef3b354e4d0c4444f2a0e3c5d2f2e547e902c06
Critiek <jamesvsomeren@gmail.com>
2024-09-17 23:56:09 +0930

    rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.yaml" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.markdown" },
+        { import = "lazyvim.plugins.extras.lang.rust" },
         -- import/override with your plugins
         { import = "plugins" },
     },
