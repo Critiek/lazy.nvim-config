50d9fbda2168badc171fa94b937f69732519b8aa
Critiek <jamesvsomeren@gmail.com>
2024-12-06 22:35:20 +1030

    termgui colors, auto checking for updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+vim.o.termguicolors = false
 
 vim.cmd.set("relativenumber")
 
