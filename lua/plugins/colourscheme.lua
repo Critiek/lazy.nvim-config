955f71fb09640dd06ce0af4eeef50cf0b91311fe
Critiek <jamesvsomeren@gmail.com>
2024-12-18 02:09:53 +1030

    updates for new version

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        "catppuccin/nvim",
-        opts = {},
-    },
+    -- {
+    --     "catppuccin/nvim",
+    --     opts = {},
+    -- },
 
     -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 
