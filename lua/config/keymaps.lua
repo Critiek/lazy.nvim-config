7e80e0215dfcfbfe3713b88c6248e43f67700529
Critiek <jamesvsomeren@gmail.com>
2024-12-18 02:17:00 +1030

    updating config for updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,9 +12,9 @@ vim.keymap.set({ "n" }, "x", '"_x')
 vim.keymap.set({ "n", "v" }, "d", '"_d')
 vim.keymap.set({ "n", "v" }, "D", '"_D')
 
-vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<C-S-v>", function()
-    vim.api.nvim_paste(vim.fn.getreg("+"), false, -1)
-end, { noremap = true, silent = true })
+-- vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<C-S-v>", function()
+--     vim.api.nvim_paste(vim.fn.getreg("+"), false, -1)
+-- end, { noremap = true, silent = true })
 
 -- vim.keymap.set({ "n", "v" }, "<leader>e", "<cmd>CHADopen<cr>")
 -- vim.keymap.set({ "n", "v" }, "<leader>e", "<cmd>Oil<cr>")
